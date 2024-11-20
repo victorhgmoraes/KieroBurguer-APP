f9f1819f8376c6f9c2ab90a866fe1ca9fee2facc
Victor <victormoraesgit@gmail.com>
2024-11-19 21:13:12 -0300

    git commit -m switch button pois n consegui fazer mais nada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,5 +53,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation ("com.google.firebase:firebase-storage:20.1.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
 }
